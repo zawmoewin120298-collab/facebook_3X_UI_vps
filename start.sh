f3587a951a6bdd42dfb758738d5673015512d6cc
zawmoewin120298-collab <zawmoewin120298@gmail.com>
2026-02-01 21:44:18 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,11 +2,10 @@
 
 echo "Starting 3x-ui..."
 cd /usr/local/x-ui/
-# x-ui ကို background မှာ run မယ်
 ./x-ui &
 
 sleep 5
 
 echo "Starting Cloudflare Tunnel..."
-# Token နဲ့ --token ကြားမှာ Space ခြားဖို့ မမေ့ပါနဲ့
-/usr/local/bin/cloudflared tunnel --no-autoupdate run --token eyJhIjoiOGQxZmJmOGJjOWM1NmFkOGYyMjZhMGM1MmNlZjgxNzIiLCJ0IjoiNzUxZDRmNTctMzZmMC00ODMwLTlmMWItNjc5ZTI0NzlmMzc3IiwicyI6Ilpqa3hNR0ZsTXpFdFpqSXlNeTAwWldGakxXRXhOV0V0TmpNeVlUTTFPR1U0TVRNeSJ9
+# ဤနေရာတွင် exec သုံးမှ Container ဆက်ပွင့်နေမှာပါ
+exec /usr/local/bin/cloudflared tunnel --no-autoupdate run --token eyJhIjoiOGQxZmJmOGJjOWM1NmFkOGYyMjZhMGM1MmNlZjgxNzIiLCJ0IjoiNzUxZDRmNTctMzZmMC00ODMwLTlmMWItNjc5ZTI0NzlmMzc3IiwicyI6Ilpqa3hNR0ZsTXpFdFpqSXlNeTAwWldGakxXRXhOV0V0TmpNeVlUTTFPR1U0TVRNeSJ9
